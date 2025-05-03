51b58076ab4395c0fdf000781b44e208a0cbc438
RohanDasari <rohandasari2000@gmail.com>
2025-05-03 22:34:24 +0530

    implemented model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformationConfig, DataTransformation
+from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
+
 
 @dataclass
 class DataIngestionConfig:
@@ -43,5 +45,9 @@ if __name__ == "__main__":
     train_data,test_data = DI.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_array, test_array,preprocessor_obj_filepath = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_array, test_array))
+
 
